dc5d4ba328855ddb29128abb7bc73d2cd0918108
sackidude <nils.nilsen9@outlook.com>
2024-05-12 19:11:56 +0200

    falling points

--- src/main.rs
+++ src/main.rs
@@ -3,47 +3,63 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .init_resource::<Game>()
+        .insert_resource(FallingTimer(Timer::from_seconds(1., TimerMode::Repeating)))
         .add_systems(Startup, setup)
         .add_systems(Update, update_score)
+        .add_systems(FixedUpdate, (move_player, spawn_falling, update_falling))
         .run();
 }
 
-#[derive(Component, Default)]
-struct Score(u32);
+#[derive(Resource, Default)]
+struct Game {
+    score: u32,
+    falling_handle: Handle<Mesh>,
+}
+
+#[derive(Component)]
+struct ScoreText;
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct Falling;
+
+#[derive(Resource)]
+struct FallingTimer(Timer);
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut game: ResMut<Game>,
 ) {
     // Camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0., 1., 6.).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(0., 1., 12.).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
     // Score text
     commands.spawn((
         TextBundle::from_sections([
-            TextSection::new(
-                "Score: ",
-                TextStyle::default()
-            ),
-            TextSection::new(
-                "0",
-                TextStyle::default()
-            ),
+            TextSection::new("Score: ", TextStyle::default()),
+            TextSection::new("0", TextStyle::default()),
         ]),
-        Score(0)
+        ScoreText,
     ));
 
     // Player
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(1., 1., 1.)),
-        material: materials.add(Color::WHITE),
-        transform: Transform::from_xyz(0.0, 0.0, 0.0),
-        ..default()
-    });
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Cuboid::new(1., 1., 1.)),
+            material: materials.add(Color::WHITE),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..default()
+        },
+        Player,
+    ));
 
     // Light
     commands.spawn(PointLightBundle {
@@ -54,11 +70,65 @@ fn setup(
         transform: Transform::from_xyz(0.0, 3.0, 1.0),
         ..default()
     });
+
+    // Ground
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Plane3d::default().mesh().size(10., 10.)),
+        material: materials.add(Color::GREEN),
+        transform: Transform::from_xyz(0., -1., 0.),
+        ..default()
+    });
+
+    // Falling object
+    game.falling_handle = meshes.add(Sphere::new(0.25));
 }
 
-fn update_score(
-    mut query: Query<(&mut Text, &mut Score)>
+fn update_score(game: Res<Game>, mut query: Query<&mut Text, With<ScoreText>>) {
+    let mut text = query.single_mut();
+    text.sections[1].value = format!("{}", game.score);
+}
+
+fn move_player(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
 ) {
-    let (mut text, score) = query.single_mut();
-    text.sections[1].value = format!("{}", score.0);
+    let mut transform = query.single_mut();
+    let mut direction = 0.;
+
+    if keyboard_input.pressed(KeyCode::ArrowLeft) {
+        direction -= 1.;
+    }
+    if keyboard_input.pressed(KeyCode::ArrowRight) {
+        direction += 1.;
+    }
+    const PLAYER_SPEED: f32 = 0.05;
+    transform.translation.x += direction * PLAYER_SPEED;
+}
+
+fn spawn_falling(
+    game: Res<Game>,
+    time: Res<Time>,
+    mut commands: Commands,
+    mut timer: ResMut<FallingTimer>,
+) {
+    // Increment the timer, if not finished, dont do anything.
+    if !timer.0.tick(time.delta()).finished() {
+        return;
+    }
+
+    commands.spawn((
+        PbrBundle {
+            mesh: game.falling_handle.clone(),
+            transform: Transform::from_xyz(0.0, 5., 0.),
+            ..default()
+        },
+        Falling,
+    ));
+}
+
+fn update_falling(mut query: Query<&mut Transform, With<Falling>>) {
+    for mut tranform in query.iter_mut() {
+        const FALLING_SPEED: f32 = 0.05;
+        tranform.translation.y -= FALLING_SPEED;
+    }
 }
